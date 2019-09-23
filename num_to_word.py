324a049fbbc77a06a7daffacbfcbab34e5a296c4
Raj Sutariya <sutariyaraj77725@gmail.com>
2019-09-23 18:08:46 +0530

    [files added]

---
+++ num_to_word.py
@@ -0,0 +1,103 @@
+"""
+Method to convert Numbers to Words
+for indian languages
+
+Use cases:-
+1) Speech recognition pre-processing
+2) Language modeling Data pre-processing
+
+-------------------------
+check indic_numbers.py to add support
+for any indian language
+"""
+from indic_numbers import supported_lang
+from indic_numbers import all_num
+from indic_numbers import num_dict
+
+
+def num_to_word(num, lang):
+    """
+    Main Method
+    :param num: Number digits from any indian language
+    :param lang: Language Code from supported Language
+    :return: UTF-8 String of numbers spoken in words
+    """
+    lang = lang.lower()
+    num = str(num)
+
+    # Load dictionary according to language code
+    assert lang in supported_lang, 'Language not supported'
+    num_dic = num_dict[lang]
+
+    # Remove punctuations from numbers
+    num = str(num).replace(',', '')
+
+    # Replace native language numbers with english digits
+    for language in supported_lang:
+        for num_index in range(10):
+            num = num.replace(all_num[language][num_index], all_num['en'][num_index])
+
+    # Assert that input contains only integer number
+    for digit in num:
+        assert digit in all_num['en'], "Give proper input"
+
+    # Process
+    # For Number longer than 9 digits
+    def all_two_digit(digits_2):
+        if len(digits_2) <= 1:                          # Provided only one/zero digit
+            return num_dic.get(digits_2, '')
+        elif digits_2 == '00':                          # Two Zero provided
+            return num_dic['0'] + ' ' + num_dic['0']
+        elif digits_2[0] == '0':                        # First digit is zero
+            return num_dic['0'] + ' ' + num_dic[digits_2[1]]
+        else:                                           # Both digit provided
+            return num_dic[digits_2]
+
+    # For Number less than 9 digits
+    def two_digit(digits_2):
+        digits_2 = digits_2.lstrip('0')
+        if len(digits_2) != 0:
+            return num_dic[digits_2]
+        else:
+            return ''
+
+    def all_digit(digits):
+        digits = digits.lstrip('0')
+        if len(digits) == 9:
+            return num_dic[digits[:2]] + num_dic['10000000'] + ' ' + all_digit(digits[2:])
+        elif len(digits) == 8:
+            return num_dic[digits[:1]] + num_dic['10000000'] + ' ' + all_digit(digits[1:])
+        elif len(digits) == 7:
+            return num_dic[digits[:2]] + num_dic['100000'] + ' ' + all_digit(digits[2:])
+        elif len(digits) == 6:
+            return num_dic[digits[:1]] + num_dic['100000'] + ' ' + all_digit(digits[1:])
+        elif len(digits) == 5:
+            return num_dic[digits[:2]] + num_dic['1000'] + ' ' + all_digit(digits[2:])
+        elif len(digits) == 4:
+            return num_dic[digits[:1]] + num_dic['1000'] + ' ' + all_digit(digits[1:])
+        elif len(digits) == 3:
+            return num_dic[digits[:1]] + num_dic['100'] + ' ' + two_digit(digits[1:])
+        else:
+            return two_digit(digits)
+
+    num = num.lstrip('0')
+    digit_len = len(num)
+
+    if digit_len == 0:
+        output = num_dic['0']
+    elif digit_len <= 9:
+        output = all_digit(num)
+    else:
+        iteration = round(digit_len/2)
+        output = all_two_digit(num[:2])  # First to digit
+        for i in range(1, iteration):
+            output = output + ' ' + all_two_digit(num[i*2:(i+1)*2])  # Next two digit pair
+        output = output + ' ' + all_two_digit(num[iteration*2:])  # Last one_digit/two_digits
+
+    output = output.strip()
+
+    # Language Specific Exception will come here
+    output = output.replace('બેસો', 'બસ્સો')
+    output = output.replace('छःसौ', 'छसौ')
+
+    return output
---
+++ test.py
@@ -0,0 +1,26 @@
+import random
+from tabulate import tabulate
+from num_to_word import num_to_word, supported_lang
+
+# test with user input numbers
+# number = '0'
+# while number != '':
+#     number = input("Enter Num: ")
+#     print(num_to_word(number, lang='gu'))
+
+
+# test module with random integer
+data_list = []
+header = ['num']
+for _ in range(5):
+    current_range = 1
+    for _ in range(12):
+        current_range = current_range*10
+        i = random.randint(current_range, current_range*10)
+        one_data = [i]
+        for lang in supported_lang:
+            one_data.append(num_to_word(i, lang=lang))
+            header.append(lang)
+        data_list.append(one_data)
+
+# print(tabulate(data_list, headers=header))
